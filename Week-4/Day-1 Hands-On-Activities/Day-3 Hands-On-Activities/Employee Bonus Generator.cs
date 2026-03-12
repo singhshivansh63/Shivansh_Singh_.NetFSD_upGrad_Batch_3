ba843f69fb1fbb549c6fe4452a71674991c489eb
Shivansh Singh <shivanshsinghaps@gmail.com>
2026-03-13 00:53:25 +0530

    Add files via upload

---
+++ Week-4/Day-1 Hands-On-Activities/Day-3 Hands-On-Activities/Employee Bonus Generator.cs
@@ -0,0 +1,44 @@
+﻿namespace Employee_Bonus_Calculator
+{
+    class Program
+    {
+        static void Main()
+        {
+            string name;
+            double salary, bonus = 0;
+            int experience;
+
+            Console.Write("Enter Name: ");
+            name = Console.ReadLine();
+
+            Console.Write("Enter Salary: ");
+            salary = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter Experience (years): ");
+            experience = int.Parse(Console.ReadLine());
+
+             
+            if (experience < 2)
+            {
+                bonus = salary * 0.05;
+            }
+            else if (experience >= 2 && experience <= 5)
+            {
+                bonus = salary * 0.10;
+            }
+            else
+            {
+                bonus = salary * 0.15;
+            }
+ 
+
+            double finalSalary = (bonus > 0) ? salary + bonus : salary;
+
+            Console.WriteLine("\nEmployee: " + name);
+            Console.WriteLine("Bonus: " + bonus.ToString("C"));
+            Console.WriteLine("Final Salary: " + finalSalary.ToString("C"));
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Week-4/Day-1 Hands-On-Activities/Day-3 Hands-On-Activities/Simple Calculator.cs
@@ -0,0 +1,52 @@
+﻿namespace Simple_Calculator
+{
+    class Program
+    {
+        static void Main()
+        {
+            double num1, num2;
+            char op;
+
+            Console.WriteLine("Enter First Number:");
+            num1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Second Number:");
+            num2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter Operator (+, -, *, /):");
+            op = char.Parse(Console.ReadLine());
+
+            switch (op)
+            {
+                case '+':
+                    Console.WriteLine("Result: " + (num1 + num2));
+                    break;
+
+                case '-':
+                    Console.WriteLine("Result: " + (num1 - num2));
+                    break;
+
+                case '*':
+                    Console.WriteLine("Result: " + (num1 * num2));
+                    break;
+
+                case '/':
+                    if (num2 == 0)
+                    {
+                        Console.WriteLine("Error: Division by Zero is not allowed");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Result: " + (num1 / num2));
+                    }
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid Operator!");
+                    break;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
