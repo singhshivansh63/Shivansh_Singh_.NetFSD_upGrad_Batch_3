7e77bf0880d072c3f00dc05028f146f870980dd8
Shivansh Singh <shivanshsinghaps@gmail.com>
2026-03-12 09:16:09 +0530

    Add files via upload

---
+++ Week-3/Day-3 Hands-On-Activities/Nuber Analysis Using Loop.cs
@@ -0,0 +1,36 @@
+﻿namespace Number_Analysis_Using_Loop
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n;
+            int evenCount = 0;
+            int oddCount = 0;
+            int sum = 0;
+
+            Console.Write("Enter Number: ");
+            n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                sum += i;
+
+                if (i % 2 == 0)
+                {
+                    evenCount++;
+                }
+                else
+                {
+                    oddCount++;
+                }
+            }
+
+            Console.WriteLine("Even Count: " + evenCount);
+            Console.WriteLine("Odd Count: " + oddCount);
+            Console.WriteLine("Sum: " + sum);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Week-3/Day-3 Hands-On-Activities/Student Grade Generator.cs
@@ -0,0 +1,50 @@
+﻿namespace Student_Grade_Evaluator
+{
+    class Program
+    {
+        static void Main()
+        {
+            string name;
+            int marks;
+
+            Console.WriteLine("Enter Name:");
+            name = Console.ReadLine();
+
+            Console.WriteLine("Enter Marks (0-100):");
+            marks = int.Parse(Console.ReadLine());
+
+            if (marks < 0 || marks > 100)
+            {
+                Console.WriteLine("Invalid Marks! Please enter marks between 0 and 100.");
+            }
+            else
+            {
+                Console.WriteLine("Student: " + name);
+
+                if (marks >= 90)
+                {
+                    Console.WriteLine("Grade: A");
+                }
+                else if (marks >= 75)
+                {
+                    Console.WriteLine("Grade: B");
+                }
+                else if (marks >= 60)
+                {
+                    Console.WriteLine("Grade: C");
+                }
+                else if (marks >= 40)
+                {
+                    Console.WriteLine("Grade: D");
+                }
+                else
+                {
+                    Console.WriteLine("Grade: Fail");
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+}
