55b936288440d036e62ce25c8d65757fe696e51d
Shivansh Singh <shivanshsinghaps@gmail.com>
2026-04-15 21:56:11 +0530

    Add files via upload

---
+++ Day-3 Hands-On-Activities/CategoryService/Controllers/CategoryController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CategoryService.Services;
+using CategoryService.Models;
+
+namespace CategoryService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CategoryController : ControllerBase
+    {
+        private readonly CategoryServiceLogic _service;
+
+        public CategoryController(CategoryServiceLogic service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok(await _service.GetAll());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var data = await _service.GetById(id);
+            return data == null ? NotFound() : Ok(data);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Category category)
+        {
+            await _service.Add(category);
+            return Ok(category);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, Category category)
+        {
+            if (id != category.CategoryId) return BadRequest();
+            await _service.Update(category);
+            return Ok(category);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _service.Delete(id);
+            return Ok();
+        }
+    }
+}
---
+++ Day-3 Hands-On-Activities/ContactService/Services/ContactService.cs
@@ -0,0 +1,25 @@
+﻿using ContactService.Models;
+using ContactService.Repositories;
+
+namespace ContactService.Services
+{
+    public class ContactServiceLogic
+    {
+        private readonly IContactRepository _repo;
+
+        public ContactServiceLogic(IContactRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public Task<List<Contact>> GetAll() => _repo.GetAll();
+
+        public Task<Contact> GetById(int id) => _repo.GetById(id);
+
+        public Task Add(Contact contact) => _repo.Add(contact);
+
+        public Task Update(Contact contact) => _repo.Update(contact);
+
+        public Task Delete(int id) => _repo.Delete(id);
+    }
+}
