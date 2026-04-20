e66eb2b65f546718652eb4e1bf84a51be45d3158
Shivansh Singh <shivanshsinghaps@gmail.com>
2026-04-20 21:52:58 +0530

    Add files via upload

---
+++ Week-10/Day-1 Hands-On-Practice/CalculatorApp.Tests/CalculatorServiceTests.cs
@@ -0,0 +1,45 @@
+﻿using Moq;
+using NUnit.Framework;
+using System.Timers;
+
+[TestFixture]
+public class CalculatorServiceTests
+{
+    private Mock<ICalculatorLogger> _mockLogger;
+    private CalculatorService _service;
+
+    [SetUp]
+    public void Setup()
+    {
+        _mockLogger = new Mock<ICalculatorLogger>();
+        _service = new CalculatorService(_mockLogger.Object);
+    }
+
+    [Test]
+    public void Add_ReturnsCorrectSum()
+    {
+        int a = 5, b = 3;
+        var result = _service.Add(a, b);
+        Assert.That(result, Is.EqualTo(8));
+    }
+
+    [Test]
+    public void Add_CallsLogger()
+    {
+        _service.Add(2, 3);
+        _mockLogger.Verify(x => x.Log(It.IsAny<string>()), Times.Once);
+    }
+
+    [Test]
+    public void Divide_ReturnsCorrectResult()
+    {
+        var result = _service.Divide(10, 2);
+        Assert.That(result, Is.EqualTo(5));
+    }
+
+    [Test]
+    public void Divide_ByZero_ThrowsException()
+    {
+        Assert.Throws<DivideByZeroException>(() => _service.Divide(10, 0));
+    }
+}
